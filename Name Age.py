602cfdfea75a7da9b6828134dc32137d65fafe47
NPCODER <parmeshadhikari29@gmail.com>
2026-04-09 21:25:23 +0545

    Add files via upload

---
+++ Name Age.py
@@ -0,0 +1,10 @@
+# Ask the user for their name and age
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Print the values
+print("Name:", name)
+print("Age:", age)
+
+#By using f Strings
+print(f"Hello! {name}. Your age is {age}.")
